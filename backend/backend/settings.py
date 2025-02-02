4169aa3d229e9b600bbbc1d571419d0a0f7abfe7
gxgash <gxgash@gmail.com>
2025-02-02 19:39:06 +0300

    delete comments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
-# STATIC_ROOT = '/backend_static/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
